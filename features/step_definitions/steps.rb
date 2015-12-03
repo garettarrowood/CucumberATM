3c3ecc0bee977caeef546a1f28bf770895b20522
Garett Arrowood <garettarrowood@gmail.com>
2015-12-03 08:32:41 -0500

    Transforms, modules, and Cucumber::RbSupport::RbWorld

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -7,18 +7,30 @@ class Account
   end
 end
 
-CAPTURE_A_NUMBER = Transform(/^\$(\d+)$/) do |digits|
+class Teller
+  def withdraw_from(account, name)
+  end
+end
+
+module KnowsMyAccount
+  def my_account
+    @my_account ||= Account.new
+  end
+end
+World(KnowsMyAccount)
+
+CAPTURE_CASH_AMOUNT = Transform(/^\$(\d+)$/) do |digits|
   digits.to_i
 end
 
-Given(/^I have deposited (#{CAPTURE_A_NUMBER}) in my account$/) do |amount|
-  my_account = Account.new
+Given(/^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/) do |amount|
   my_account.deposit(amount)
-  expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount} but it was #{my_account.balance}"
+  expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount}"
 end
 
-When(/^I request \$(\d+)$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I withdraw (#{CAPTURE_CASH_AMOUNT})$/) do |amount|
+  teller = Teller.new
+  teller.withdraw_from(my_account, amount)
 end
 
 Then(/^\$(\d+) should be dispensed$/) do |arg1|
