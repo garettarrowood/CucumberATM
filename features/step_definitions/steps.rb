ebd0d09e602872e76f6df0c82977272fb5623a92
Garett Arrowood <garettarrowood@gmail.com>
2015-12-03 08:50:23 -0500

    new class and grew World

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -8,16 +8,35 @@ class Account
 end
 
 class Teller
-  def withdraw_from(account, name)
+  def initialize(cash_slot)
+    @cash_slot = cash_slot
+  end
+  def withdraw_from(account, amount)
+    @cash_slot.dispense(amount)
   end
 end
 
-module KnowsMyAccount
+class CashSlot
+  def contents
+    @contents or raise("I'm empty!")
+  end
+  def dispense(amount)
+    @contents = amount
+  end
+end
+
+module KnowsTheDomain
   def my_account
     @my_account ||= Account.new
   end
+  def cash_slot
+    @cash_slot ||= CashSlot.new
+  end
+  def teller
+    @teller ||= Teller.new(cash_slot)
+  end
 end
-World(KnowsMyAccount)
+World(KnowsTheDomain)
 
 CAPTURE_CASH_AMOUNT = Transform(/^\$(\d+)$/) do |digits|
   digits.to_i
@@ -29,10 +48,14 @@ Given(/^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/) do |amount|
 end
 
 When(/^I withdraw (#{CAPTURE_CASH_AMOUNT})$/) do |amount|
-  teller = Teller.new
   teller.withdraw_from(my_account, amount)
 end
 
-Then(/^\$(\d+) should be dispensed$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/) do |amount|
+  expect(cash_slot.contents).to eq(amount)
 end
+
+
+
+
+
